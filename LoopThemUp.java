824c5fceadb647c0b2a90e8bbad4a718beccf45d
vaidhiyanathanr <vaidhiyanathanr@cardiff.ac.uk>
2025-10-01 16:45:36 +0100

    Create LoopThemUp.java

---
+++ LoopThemUp.java
@@ -0,0 +1,14 @@
+
+public class LoopThemUp {
+    /**
+     * TODO:
+     * Write a program that prints numbers 10 to 0 using a while loop and a also a do loop.
+     * Thus, you should print 10 to 0 twice but in separate loops.
+     * Be careful with the start and end values.
+     * Once you have got that running, extend your program so that after printing 10 to 0 twice
+     * the program then calculates and prints the sum of the numbers 0 to 10,000
+     * i.e. 0+1, 1+2, 3+3, 6 + 4, ... .
+     * Use a loop for the calculation.
+     * Do you also want to find the complexity of this loop?
+     */
+}
