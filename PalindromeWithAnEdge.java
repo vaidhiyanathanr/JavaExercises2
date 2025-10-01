7bd6d1948ddb1f4c8932e4df268c0e0950afd13a
vaidhiyanathanr <vaidhiyanathanr@cardiff.ac.uk>
2025-10-01 16:45:17 +0100

    Create PalindromeWithAnEdge.java

---
+++ PalindromeWithAnEdge.java
@@ -0,0 +1,13 @@
+
+public class PalindromeWithAnEdge {
+    /**
+     * TODO:
+     * Take the Palindrome Task done previous and add the following edge case testing
+     *
+     * Empty string: Should return true.
+     * Single character string: Should return true.
+     * Strings with special characters or spaces: Should be handled correctly.
+     *
+     * Could you try to use exceptions for these? Don't worry too much if you aren't using exceptions yet
+     */
+}
