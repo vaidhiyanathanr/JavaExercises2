d29bb19cdedbb3f7af1e393ee295b03e4e2290e5
vaidhiyanathanr <vaidhiyanathanr@cardiff.ac.uk>
2025-10-01 16:44:37 +0100

    Create SpudYouLove2.java

---
+++ SpudYouLove2.java
@@ -0,0 +1,11 @@
+public class SpudYouLove2 {
+    /**
+     * TODO:
+     * Extend your code for the previous task (make a copy of it first) with further validation. If the
+     * user enters a non-numeric amount for number of toppings then the program should deal with
+     * this (it should print a helpful message and re-ask for the number of toppings).
+     * To do this, you will need to read a string, test if it actually represents a number and then parse
+     * this string as a number. You will need to research how to parse a string as a number.
+     * If you want to use an Exception (custom) please feel free to do so.
+     */
+}
